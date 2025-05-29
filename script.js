258411ee6e389e0afb6ccb2642960293d8c59d2a
foxstacks <198858136+foxstacks@users.noreply.github.com>
2025-05-28 20:09:04 -0500

    fixed alot

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const remove = document.querySelector('#remove')
 const gifsearch = document.querySelector('#gifsearch')
 const error = document.querySelector('#searcherror')
 const giftext = document.querySelector('#giftext')
+const spinner = document.querySelector('#spinner')
 
 remove.disabled = true
 
@@ -15,14 +16,17 @@ generate.addEventListener('click', () => {
     error.textContent = 'Enter a search!'
   }
   else {
+    spinner.style.display = 'block'
     giftext.style.display = 'none'
-    gifsearch.disabled = true
     remove.disabled = false
 
     const search = gifsearch.value
+    const gifcontainer = document.querySelector('.gifcontainer')
+    const existingGif = gifcontainer.querySelector('.gif')
+    if (existingGif) existingGif.remove()
+
     const img = document.createElement('img')
     img.classList.add('gif')
-    const gifcontainer = document.querySelector('.gifcontainer')
     gifcontainer.appendChild(img)
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=QyscNsnRFMb27o0WrnCDqoZQ3erDzn2J&s=${search}`, {mode: 'cors'})
       .then(function(response) {
@@ -30,6 +34,7 @@ generate.addEventListener('click', () => {
       })
       .then(function(response) {
         if (!response.data || !response.data.images) {
+          spinner.style.display = ''
           console.log(`Did not find a GIF! URL: ${response.data.images.original.url}`)
           error.textContent = 'No GIF found for that search.'
           img.remove()
@@ -38,6 +43,7 @@ generate.addEventListener('click', () => {
           remove.disabled = true
           return
         }
+        spinner.style.display = ''
         console.log(`Found GIF! URL: ${response.data.images.original.url}`)
         img.src = response.data.images.original.url
       })
@@ -46,7 +52,6 @@ generate.addEventListener('click', () => {
 
 remove.addEventListener('click', () => {
   giftext.style.display = 'block'
-  gifsearch.disabled = false
   gifsearch.value = ''
   remove.disabled = true
   const img = document.querySelector('.gif')
